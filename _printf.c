2d9d2f19601c813214a6c9fb2eade5a7356efaee
Abenezer Tesfaye <105986912+Abentesfaye@users.noreply.github.com>
2023-03-26 21:39:54 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,55 +1,57 @@
 #include "main.h"
 /**
- *_printf - function that work like a printf function
- *@format: the format that pass to the function
- *Return: length of format
+ * _printf - custom printf function that handles only %c and %s specifiers
+ * @format: format string to print
+ * @...: variable arguments list
+ *
+ * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	int counter = 0;
-	va_list args;
+	char *str
+    int counter = 0;
+    va_list args;
 
-	va_start(args, format);
-	if (format == NULL)
-		return -1;
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
+    va_start(args, format);
 
-			switch (*format)
-			{
-				case 'c':
-					{
-						_printChar(va_arg(args, int));
-						break;
-					}
-				case 's':
-					{
-						_printStr(va_arg(args, char *));
-						break;
-					}
-				case '%':
-					{
-						_printChar('%');
-						break;
-					}
-				default:
-					{
-						_printChar(*format);
-						break;
-					}
-			}
-		}
-		else
-		{
-			_printChar(*format);
-		}
-		format++;
-		counter++;
-	}
-	va_end(args);
-	return (counter);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            switch (*format)
+            {
+                case 'c':
+                    {
+                      _printChar(va_arg(args, int));
+                        counter++;
+                        break;
+                    }
+                case 's':
+                    {
+                        str = va_arg(args, char *);
+                        counter += _printStr(str);
+                        break;
+                    }
+                default:
+                    {
+                        _printChar(*format);
+                        counter++;
+                        break;
+                    }
+            }
+        }
+        else
+        {
+            _printChar(*format);
+            counter++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return (counter);
 }
