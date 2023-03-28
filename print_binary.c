f76f0143e609d8d1220009bbd98ebbdd938a7666
“Lunahar21” <kindinewd@gmail.com>
2023-03-28 13:10:32 -0700

    _printf

--- print_binary.c
+++ print_binary.c
@@ -8,21 +8,20 @@
  */
 int _printBinary(va_list args)
 {
-	 unsigned int n = va_arg(args, unsigned int);
-    int binary[32];
-    int i, len = 0;
+	unsigned int n = va_arg(args, unsigned int);
+	int binary[32];
+	int i, len = 0;
 
-    for (i = 0; n > 0; i++)
-    {
-        binary[i] = n % 2;
-        n /= 2;
-    }
 
-    for (i = i - 1; i >= 0; i--)
-    {
-        _printChar(binary[i] + '0');
-        len++;
-    }
-
-    return (len);
+	for (i = 0; n > 0; i++)
+	{
+		binary[i] = n % 2;
+		n /= 2;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_printChar(binary[i] + '0');
+		len++;
+	}
+	return (len);
 }
