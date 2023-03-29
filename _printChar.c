eeb35ad6a1ab752635615998acab403a566ab9ee
Abentesfaye <abentesfaye11@gmail.com>
2023-03-29 06:24:51 +0300

    hot fix

--- _printChar.c
+++ _printChar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "main.h"
 /**
  * _printChar - Write a character to stdout
  * @c: The character to write
--- print_binary.c
+++ print_binary.c
@@ -1,22 +1,34 @@
 #include "main.h"
 
 /**
- * _printBinary - Converts a decimal number to binary and prints it.
+ * _printBinary - prints the binary representation of a number
+ * @args: the va_list containing the number to print
  *
- * @num: The decimal number to be converted.
- *
- * Return: The length of the binary representation.
+ * Return: the number of digits printed
  */
-int _printBinary(int num)
+int _printBinary(va_list args)
 {
+	 int num = va_arg(args, int);
+	int mask = 1;
 	int length = 0;
+	int i;
 
-	if (num > 1)
+	/* count the number of digits in the binary string */
+	for (i = 0; i < 32; i++)
 	{
-		length = _printBinary(num >> 1);
+		if ((num & mask) == mask)
+			length++;
+		mask <<= 1;
 	}
-	_printChar((num & 1) + '0');
-	length++;
-	return (length);
+
+	/* print the binary string */
+	for (i = 0; i < length; i++)
+	{
+		if ((num & (1 << (length - i - 1))) == (1 << (length - i - 1)))
+			_printChar('1');
+		else
+			_printChar('0');
+	}
+
+	return (length - 1);
 }
->>>>>>> 5628b91108367a7d5dbe4a0bfd23c05f16948744
