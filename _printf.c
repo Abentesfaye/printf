40b90a273a9bcfbec9fd798f20198eb6ba3491bf
Abentesfaye <abentesfaye11@gmail.com>
2023-03-31 05:50:16 +0300

    fix

--- _printf.c
+++ _printf.c
@@ -9,50 +9,42 @@
  */
 int _printf(const char *format, ...)
 {
+	int i = 0, j = 0;
 	int (*f)(va_list);
-	int i = 0;
-	int counter = 0;
-	int value = 0;
 	va_list args;
 
 	va_start(args, format);
-	
-	/*prvent parsing a null pointer*/
-
-	if (format == NULL)
+	if (format == NULL || !format[i + 1])
 		return (-1);
-	/*print each character of string*/
 	while (format[i])
 	{
-		if (format[i] != '%')
-		{
-		value = write(1, &format[i], 1);
-		counter += value;
-		i++;
-		continue;
-		}
 		if (format[i] == '%')
 		{
-			f = get_func(&format[i + 1]);
-			if (f != NULL)
+			if (format[i + 1])
 			{
 				if (format[i + 1] != 'c' && format[i + 1] != 's'
 				&& format[i + 1] != '%' && format[i + 1] != 'd'
-				&& format[i + 1] != 'i' && format[i + 1] != 'b'
-				&& format[i + 1] != 'p')
+				&& format[i + 1] != 'i')
 				{
-					counter += _printChar(format[i]); /* % */
-					counter += _printChar(format[i + 1]); /* d*/
-					i++;  /* d */
+					j += _putchar(format[i]);
+					j += _putchar(format[i + 1]);
+					i++;
 				}
 				else
 				{
 					f = get_func(&format[i + 1]);
-					counter += f(args);
+					j += f(args);
 					i++;
 				}
 			}
 		}
+		else
+		{
+			_putchar(format[i]);
+			j++;
+		}
+		i++;
 	}
-	return (counter);
+	va_end(args);
+	return (j);
 }
